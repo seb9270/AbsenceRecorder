75156c94ad436b127353b4e3d6ced1c70492599c
Sebastian Micko <Micko.S@etoncollege.org.uk>
2024-02-21 21:47:52 +0000

    Initial Commit

---
+++ AbsenceRecorder/AbsenceRecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  AbsenceRecorderApp.swift
+//  AbsenceRecorder
+//
+//  Created by Micko, Sebastian (AMM) on 21/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct AbsenceRecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
